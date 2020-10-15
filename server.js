057c29800fc855da7cd5072b2c0bda1b05981b9c
Erdal SATIK <erdalstk@gmail.com>
2020-10-15 04:51:58 +0300

    Added CORS config
    [+] Add cors config for api

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+var cors = require('cors');
+
 const server = express();
 
 const body_parser = require("body-parser");
@@ -14,6 +16,22 @@ const db = require("./db");
 const dbName = "performance";
 const collectionName = "loadtimes";
 
+
+
+var allowlist = process.env.CORS_ALLOWED_LIST;
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+
+
+
 // db init
 db.initialize(
   dbName,
@@ -21,7 +39,7 @@ db.initialize(
   function (dbCollection) {
 
     // POST
-    server.post("/loadtimes", (request, response) => {
+    server.post("/loadtimes", cors(corsOptionsDelegate), (request, response) => {
       const item = request.body;
       dbCollection.insertOne(item, (error, result) => {
         if (error) throw error;
@@ -33,7 +51,7 @@ db.initialize(
     });
 
     // GET ALL
-    server.get("/loadtimes", (request, response) => {
+    server.get("/loadtimes", cors(corsOptionsDelegate), (request, response) => {
       dbCollection.find().toArray((_error, _result) => {
         if (_error) throw _error;
         response.json(_result);
@@ -41,7 +59,7 @@ db.initialize(
     });
 
     // GET BY ID
-    server.get("/loadtimes/:id", (request, response) => {
+    server.get("/loadtimes/:id", cors(corsOptionsDelegate), (request, response) => {
       const itemId = request.params.id;
       dbCollection.findOne({ _id: ObjectId(itemId) }, (error, result) => {
         if (error) throw error;
@@ -50,7 +68,7 @@ db.initialize(
     });
 
     // DELETE BY ID
-    server.delete("/loadtimes/:id", (request, response) => {
+    server.delete("/loadtimes/:id", cors(corsOptionsDelegate), (request, response) => {
       const itemId = request.params.id;
 
       dbCollection.deleteOne({ id: itemId }, function (error, result) {
