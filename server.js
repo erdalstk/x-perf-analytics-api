00324be7bf5c2b82e1b00f140703bbe81a726c64
Erdal SATIK <erdalstk@gmail.com>
2020-10-15 04:59:25 +0300

    Process env cors allow list update
    Process env cors allow list update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const collectionName = "loadtimes";
 var allowlist = process.env.CORS_ALLOWED_LIST;
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+  if (Array.from(allowlist).indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
   } else {
     corsOptions = { origin: false } // disable CORS for this request
